bc82d55105aea5f714547154babbbf4d83a67417
chibuzorO <chibuzor.obiora@andela.co>
2015-07-08 16:31:01 +0100

    Created basic file logic with appropriate names to help with code structure and logic flow

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,2 @@
-'use strict';
-var help = require('./help');
-
-module.exports = function(robot) {
-  robot.hear(/cally/i, function (res) {
-    res.send('Hi My name is Cally and I am here to remind you when your leave will start and other important dates.');
-  });
-  // robot.respond(/help/i, function (res){
-  //   res.send(help.join);
-  // });
-};
+// REQUIRE ALL THE OTHER FILES
+// CORE LOGIC FOR THE APP

9396ad84c07360f48c91f0bb01c583c29172da5e
chibuzorO <chibuzor.obiora@andela.co>
2015-07-26 22:40:06 +0100

    Spec'd out tasks for calli-interview

--- scripts/interview.js
+++ scripts/interview.js
@@ -1,2 +1,21 @@
-// THIS FILE WILL HOUSE ALL THE LOGIC FOR INTERVIEW DATES 
-// EXPORT ALL FUNCTIONS
+/***
+This file would contain all the logic for implementing the use cases for the interview bookings
+
+Book a date. (abstract this function for everyone) from slack
+1. will be responsible for scheduling meetings between Fellows who are joining the interviewers team and the trainers,  
+2. will pitch fellows and trainers according to their choice of dates  and 
+3. will also send out reminders to fellows and trainers of their scheduled meeting 
+4. will send notifications to the admin on regular basis
+
+
+fellow completes training and joins interview-team 
+admin adds fellow by slack handles to the cali-interview array. 
+fellow gets message from cally telling her that she has been added to the interview - team
+   and would be getting notoifications for such
+next up is the fellow gets a notification from the for the list of available dates [mapped to a string(TRAINER/SLOT)]
+fellow chooses a slot and sends to calli
+calli uses the slot to create a calendar event for that fellow with that trainer
+calli sends reminders to the fellows when the day is coming. 
+  :--(abtract this method out so that is the same as the one that reminds the birthday,  leave and the rest )
+*/
+

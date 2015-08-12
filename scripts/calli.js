648f1f5c4206f8c3ea451a6d031b28b9b8fae28e
chibuzorO <chibuzor.obiora@andela.co>
2015-08-12 12:01:40 +0100

    Show accurate help prompts on help command
    The help command was previously triggering hubot's defaults. This
    was commented out and our own code put in place. The goal to use
    calli.js as main
    entry point is also being put in place.

--- scripts/calli.js
+++ scripts/calli.js
@@ -1,11 +1,27 @@
 // REQUIRE ALL THE OTHER FILES
 // CORE LOGIC FOR THE APP
 var interview = require('./interview');
+var help = require('./help');
 
 
 module.exports = function(robot) {
-    /**INTERVIEW**/
-    robot.respond(/add: (.*)/, function() {
-        interview.addFellowToInterviewTeam(fellow);
+
+    robot.respond(/help/i, function(res) {
+        var emit;
+        emit = help.join("\n");
+        res.send(emit);
     });
+
+    robot.respond(/hi Cally || hi/i, function(res) {
+        var response = 'Hi ' + res.message.user.name + ', My name is Cally and I am here to' +
+                        'remind you when your leave will start and other important dates.';
+        res.send(response);
+    });
+
+
+
+    /**INTERVIEW**/
+    // robot.respond(/add: (.*)/, function() {
+    //     interview.addFellowToInterviewTeam(fellow);
+    // });
 };

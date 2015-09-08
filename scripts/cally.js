cd3da46ecf75aec6b5ac503cd7681e6418287d16
chibuzorO <chibuzor.obiora@andela.co>
2015-09-08 10:39:33 +0100

    Refactor leave.js adding functionality to cally.js

--- scripts/cally.js
+++ scripts/cally.js
@@ -1,4 +1,5 @@
 var interview = require('./interview');
+var leave = require('./leave');
 var userdb = require('./userdb');
 var help = require('./help');
 
@@ -16,11 +17,32 @@ module.exports = function(robot) {
         res.send(response);
     });
 
+
+    /**
+     * LEAVE FUNCTIONALITY
+     */
+    robot.respond(/list-all/i, function(res) {
+        leave.listAll('G064YFGG1', res.message.user.id, res);
+    });
+
+    robot.respond(/show|sml|show-my-leave/i, function(res) {
+        leave.showOne(res.message.user.name, res.message.user.email_address, res, robot);
+    });
+
+
+    /**
+     * INTERVIEW FUNCTIONALITY
+     */
     robot.respond(/add: (.*)/, function(res) {
         var response = 'Hi ' + res.match[1];
         var slack_id = res.match[1];
         res.send(response);
     });
+
+
+    /**
+     * DB FUNCTIONALITY
+     */
 
     robot.respond(/update/i, function(res) {
         userdb.update();
--- scripts/leave.js
+++ scripts/leave.js
@@ -5,6 +5,9 @@ var moment = require('moment');
 var CronJob = require('cron').CronJob;
 var bluebird = require('bluebird');
 
+var exports = module.exports = {};
+//Function helpers
+
 // Polyfill in other to use includes.
 String.prototype.includes = function(value) {
     return this.indexOf(value) !== -1;
@@ -33,174 +36,6 @@ if (!Array.prototype.find) {
     };
 }
 
-module.exports = function(robot) {
-    //Admin can get the list of all calendar events
-    robot.respond(/list-all/i, function(res) {
-        var channel = 'G064YFGG1'; // This is test-cally channel id
-        var isMemberPromise = slackApi.getGroup(channel, res.message.user.id).then(function(isMember) {
-            return (isMember !== -1);
-        });
-
-        var isAdminPromise = slackApi.getUserFromSlack(res.message.user.id);
-        bluebird.all([isMemberPromise, isAdminPromise]).spread(function(isMember, isAdmin) {
-            if (isMember || isAdmin) {
-                return googleApi.getAllDates();
-            } else {
-                return [];
-            }
-        }).then(function(dates) {
-            var messages = dates.map(function(date) {
-                var name = date.summary.match(/^[^(]*/)[0];
-                var status = date.status;
-                var startDates = date.start.date || date.start.dateTime;
-                var endDate = date.end.date || date.end.dateTime;
-                return '```' + name + ' - from ' + getDayOfTheWeek(startDates) + ' - to ' + getDayOfTheWeek(endDate) + ' - ' + status + '```';
-            });
-            res.send(messages.join('\n'));
-        }).catch(function(err) {
-            console.log('Error', err.stack);
-        });
-    });
-
-
-    // get single user leave date
-    // a Single user can check his/her own leave date
-    robot.respond(/show|sml|show-my-leave/i, function(res) {
-        var userName = res.message.user.name;
-        var names = res.message.user.email_address.match(/^[^@]+/)[0].split('.')
-        googleApi.getAllDates().then(function(data) {
-            return data.filter(function(x) {
-                var name = x.summary
-                    .match(/^[^(]*/)[0]
-                    .trim().toLowerCase();
-
-                return names.every(function(y) {
-                    return name.includes(y);
-                });
-            })
-        }).then(function(result) {
-            var message = result.map(function(data) {
-                var status = data.status;
-                var startDate = data.start.date || data.start.dateTime;
-                var endDate = data.end.date || data.end.dateTime;
-                return "Start Day:" + getDayOfTheWeek(startDate) + ". End Date: " + getDayOfTheWeek(endDate) + ",  Status :" + status;
-            })
-            return (message.join('\n'));
-        }).then(function(result) {
-            if (!result) {
-                res.send("I'm sorry " + res.message.user.name + ", it seems your leave date has not been registered. kindly contact people-intern@andela.com for more information.")
-                return;
-            }
-            var msg = "Hey " + res.message.user.name + " trust you are doing well :smile: this is the information I found about your leave :\n" + result + " \n Contact people-intern@andela.co for more info.";
-            robot.emit('slack-attachment', {
-                content: {
-                    fallback: msg,
-                    pretext: "Leave Request Information",
-                    text: msg,
-                    color: '#7CD197'
-                },
-                channel: userName
-            });
-        });
-
-    });
-
-    //Get todays date and calculate one month from now.
-    var sendLeaveNotice = function(dateToQuery, duration) {
-        var namesPromise = googleApi.getAllDates().then(function(leavedates) {
-
-            return leavedates.filter(function(event) {
-                return event.start.date === dateToQuery;
-            });
-        }).then(function(todaysLeavenotice) {
-
-            return todaysLeavenotice.map(function(obj) {
-                var name = obj.summary
-                    .match(/^[^(]*/)[0]
-                    .trim().toLowerCase();
-                return name.replace(' ', '.');
-            });
-        });
-        var slackusersPromise = slackApi.getAllSlackMembers().then(function(slackusers) {
-            return slackusers.map(function(x) {
-                return [
-                    x.email.match(/^[^@]+/)[0],
-                    x.name,
-                    x.real_name,
-                    x.real_name.toLowerCase().replace(' ', '.'),
-                ];
-            });
-        });
-
-        bluebird.all([namesPromise, slackusersPromise]).spread(function(ids, idToRoomNames) {
-                var weirdNames = {
-                    'ijeoma.arisah': ['', 'jay', 'Jay']
-                };
-                ids.map(function(id) {
-                        if (weirdNames[id]) return weirdNames[id];
-                        var revid = id.split('.').reverse().join('.');
-                        var y = idToRoomNames.find(function(idWithRoomName) {
-                            var currid = idWithRoomName[0];
-                            var curridbackup = idWithRoomName[3];
-
-                            return id === currid || revid === currid || id === curridbackup || revid === curridbackup || levenshteinDistance(id, currid) < 3;
-                        });
-                        if (y === undefined) {
-                            console.log(y)
-                                //   var msg = y + " could not be found in the database it could be a wrong spelling. kindly check the user information"
-                                // robot.send({
-                                //   room: 'i-cally-bot'
-                                // }, msg )
-                        }
-                        return y;
-                    })
-                    .filter(function(x) {
-                        return x !== undefined;
-                    })
-                    .map(function(x) {
-                        return x[1];
-                    })
-                    .forEach(function(name) {
-                        var msg = "Hey " + name + ", your leave is coming up on " + duration + " from now, " + getDayOfTheWeek(dateToQuery) + ", If you haven’t, please inform your client/trainer/line manager of your upcoming leave. Upon approval, forward to people-intern@andela.com. Thank you. :smile:";
-                        robot.emit('slack-attachment', {
-                            content: {
-                                fallback: msg,
-                                pretext: "Personal Holiday Information",
-                                text: msg,
-                                color: '#7CD197'
-                            },
-                            channel: name
-                                // channel: "adebayo.m"
-                        });
-                    });
-            })
-            .catch(function(err) {
-                console.log('err', err.stack);
-            });
-    };
-
-    /*
-     * a wrapper function for the cron jobs
-     * @param ({}, string)
-     * Output : calls another function
-     */
-    function createJob(deltaTime, duration) {
-        return function g() {
-            var time = moment().add(deltaTime).format('YYYY-MM-DD');
-            sendLeaveNotice(time, duration);
-        };
-    }
-
-    new CronJob('00 00 11 * * *', createJob({
-        months: 1
-    }, "One Month"), null, true);
-    new CronJob('00 30 11 * * *', createJob({
-        weeks: 2
-    }, "2 weeks"), null, true);
-    // new CronJob('00 30 10 * * *', createJob({days: 3}, "3 days"), null, true);
-
-};
-
 /*
  * returns a formated year from the
  * @param (2015-08-20)
@@ -215,7 +50,6 @@ function getDayOfTheWeek(day) {
  * @input (two items to compare)
  * Output : returns boolean;
  */
-
 function levenshteinDistance(a, b) {
     if (a.length === 0) return b.length;
     if (b.length === 0) return a.length;
@@ -249,3 +83,167 @@ function levenshteinDistance(a, b) {
 
     return matrix[b.length][a.length];
 }
+
+
+exports.listAll = function(channelId, userId, res) {
+    var isMemberPromise = slackApi.getGroup(channelId, userId).then(function(isMember) {
+        return (isMember !== -1);
+    });
+    var isAdminPromise = slackApi.getUserFromSlack(userId);
+
+    bluebird.all([isMemberPromise, isAdminPromise]).spread(function(isMember, isAdmin) {
+        if (isMember || isAdmin) {
+            return googleApi.getAllDates();
+        } else {
+            return [];
+        }
+    }).then(function(dates) {
+        var messages = dates.map(function(date) {
+            var name = date.summary.match(/^[^(]*/)[0];
+            var status = date.status;
+            var startDates = date.start.date || date.start.dateTime;
+            var endDate = date.end.date || date.end.dateTime;
+            return '```' + name + ' - from ' + getDayOfTheWeek(startDates) + ' - to ' + getDayOfTheWeek(endDate) + ' - ' + status + '```';
+        });
+        res.send(messages.join('\n'));
+    }).catch(function(err) {
+        console.log('Error', err.stack);
+    });
+};
+
+exports.showOne = function(userName, userEmailAdress, res, robot) {
+    var names = userEmailAdress.match(/^[^@]+/)[0].split('.');
+
+    googleApi.getAllDates().then(function(data) {
+        return data.filter(function(x) {
+            var name = x.summary
+                .match(/^[^(]*/)[0]
+                .trim().toLowerCase();
+
+            return names.every(function(y) {
+                return name.includes(y);
+            });
+        });
+    }).then(function(result) {
+        var message = result.map(function(data) {
+            var status = data.status;
+            var startDate = data.start.date || data.start.dateTime;
+            var endDate = data.end.date || data.end.dateTime;
+            return "Start Day:" + getDayOfTheWeek(startDate) + ". End Date: " + getDayOfTheWeek(endDate) + ",  Status :" + status;
+        });
+        return (message.join('\n'));
+    }).then(function(result) {
+        if (!result) {
+            res.send("I'm sorry " + userName + ", it seems your leave date has not been registered. kindly contact people-intern@andela.com for more information.")
+            return;
+        }
+        var msg = "Hey " + userName + " trust you are doing well :smile: this is the information I found about your leave :\n" + result + " \n Contact people-intern@andela.co for more info.";
+
+        robot.emit('slack-attachment', {
+            content: {
+                fallback: msg,
+                pretext: "Leave Request Information",
+                text: msg,
+                color: '#7CD197'
+            },
+            channel: userName
+        });
+    });
+};
+
+
+
+
+//Get todays date and calculate one month from now.
+//Used by the Cron Job in createJob
+var sendLeaveNotice = function(dateToQuery, duration) {
+    var namesPromise = googleApi.getAllDates().then(function(leavedates) {
+        return leavedates.filter(function(event) {
+            return event.start.date === dateToQuery;
+        });
+    }).then(function(todaysLeavenotice) {
+
+        return todaysLeavenotice.map(function(obj) {
+            var name = obj.summary
+                .match(/^[^(]*/)[0]
+                .trim().toLowerCase();
+            return name.replace(' ', '.');
+        });
+    });
+
+    var slackusersPromise = slackApi.getAllSlackMembers().then(function(slackusers) {
+        return slackusers.map(function(x) {
+            return [
+                x.email.match(/^[^@]+/)[0],
+                x.name,
+                x.real_name,
+                x.real_name.toLowerCase().replace(' ', '.'),
+            ];
+        });
+    });
+
+    bluebird.all([namesPromise, slackusersPromise]).spread(function(ids, idToRoomNames) {
+            var weirdNames = {
+                'ijeoma.arisah': ['', 'jay', 'Jay']
+            };
+            ids.map(function(id) {
+                    if (weirdNames[id]) return weirdNames[id];
+                    var revid = id.split('.').reverse().join('.');
+                    var y = idToRoomNames.find(function(idWithRoomName) {
+                        var currid = idWithRoomName[0];
+                        var curridbackup = idWithRoomName[3];
+                        return id === currid || revid === currid || id === curridbackup || revid === curridbackup || levenshteinDistance(id, currid) < 3;
+                    });
+                    if (y === undefined) {
+                        console.log(y);
+                        //   var msg = y + " could not be found in the database it could be a wrong spelling. kindly check the user information"
+                        // robot.send({
+                        //   room: 'i-cally-bot'
+                        // }, msg )
+                    }
+                    return y;
+                })
+                .filter(function(x) {
+                    return x !== undefined;
+                })
+                .map(function(x) {
+                    return x[1];
+                })
+                .forEach(function(name) {
+                    var msg = "Hey " + name + ", your leave is coming up on " + duration + " from now, " + getDayOfTheWeek(dateToQuery) + ", If you haven’t, please inform your client/trainer/line manager of your upcoming leave. Upon approval, forward to people-intern@andela.com. Thank you. :smile:";
+                    robot.emit('slack-attachment', {
+                        content: {
+                            fallback: msg,
+                            pretext: "Personal Holiday Information",
+                            text: msg,
+                            color: '#7CD197'
+                        },
+                        channel: name
+                    });
+                });
+        })
+        .catch(function(err) {
+            console.log('err', err.stack);
+        });
+};
+
+/*
+ * a wrapper function for the cron jobs
+ * @param ({}, string)
+ * Output : calls another function
+ */
+function createJob(deltaTime, duration) {
+    return function g() {
+        var time = moment().add(deltaTime).format('YYYY-MM-DD');
+        sendLeaveNotice(time, duration);
+    };
+}
+
+
+new CronJob('00 00 11 * * *', createJob({
+    months: 1
+}, "One Month"), null, true);
+new CronJob('00 30 11 * * *', createJob({
+    weeks: 2
+}, "2 weeks"), null, true);
+// new CronJob('00 30 10 * * *', createJob({days: 3}, "3 days"), null, true);

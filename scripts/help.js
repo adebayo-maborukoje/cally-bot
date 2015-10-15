ff0e89d85421c4fe6868d0fee5b27fb844d5b21a
chibuzorO <chibuzor.obiora@andela.co>
2015-10-15 12:59:54 +0100

    Update help commands with delete feature

--- scripts/help.js
+++ scripts/help.js
@@ -2,5 +2,6 @@ module.exports = [
     '`cally` - Introduces you to Cally, the calendar bot. Please type `help` to know more about how to operate Cally. ',
     '`list-all` - *(Admin Only)* This will list all the yet to be taken leave days for the fellows and staff.',
     '`show` - Will show your scheduled leave days',
-    '`set leave`: create leave in the fellows leave calendar with format *Name*, *Start Date(YYYY-MM-DD)*, *End Date (YYYY-MM-DD)*'
+    '`create leave: Name, Start Date(YYYY-MM-DD), End Date (YYYY-MM-DD)`create leave in the fellows leave calendar with that format',
+    '`remove leave: email address, leave_start_date` will remove the leave from the calendar'
 ];

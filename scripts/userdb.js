9cf2d1514c8eb8f8f467c367fcb1290a7c2aef34
chibuzorO <chibuzor.obiora@andela.co>
2015-10-15 10:15:55 +0100

    Add flag field to leave event
    Added a boolean field to the leave event model that will be resolved on
    save when the leave event is created. This will help future streamline
    future queries as the field would help determing if date is in the
    future (hasBeenTaken=false) or in the past (hasBeenTaken=true).

--- scripts/userdb.js
+++ scripts/userdb.js
@@ -42,13 +42,17 @@ var LeaveEvent = new Schema({
     googleEventId: String,
     startDate: Date,
     endDate: Date,
-    numberOfDaysBooked: Number
+    numberOfDaysBooked: Number,
+    hasBeenTaken: Boolean  //flag
 });
 var LeaveEvent = mongoose.model('LeaveEvent', LeaveEvent);
 
 
 exports.saveLeaveEvent = function(eventObject, res) {
     var leaveEvent = new LeaveEvent();
+    var momentStartDate = moment(eventObject.startDate.replace('-',' '), 'YYYY MM DD'),
+        momentEndDate = moment(eventObject.endDate.replace('-',' '), 'YYYY MM DD'),
+        dur = momentEndDate.diff(momentStartDate);
 
     leaveEvent.userId = eventObject.userId
     leaveEvent.slackId = eventObject.slackId;
@@ -56,9 +60,9 @@ exports.saveLeaveEvent = function(eventObject, res) {
     leaveEvent.googleEventId = eventObject.googleEventId;
     leaveEvent.startDate = eventObject.startDate;
     leaveEvent.endDate = eventObject.endDate;
-
-    var dur = moment(eventObject.endDate.replace('-',' '), 'YYYY MM DD').diff(moment(eventObject.startDate.replace('-',' '), 'YYYY MM DD'));
     leaveEvent.numberOfDaysBooked = moment.duration(dur).asDays();
+    leaveEvent.hasBeenTaken = moment().diff(momentStartDate) > 1; //true if today is in the future of the start date.
+    //save to the database.
     leaveEvent.save();
     res.send("done")
 };
